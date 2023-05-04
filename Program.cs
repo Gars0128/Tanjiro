14c2bc5b1f32ed6f330076b7b67e9d778c156881
Gars0128 <70803438+Gars0128@users.noreply.github.com>
2023-05-04 14:42:19 +0200

    Add files via upload
    factorial number

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Threading;
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine());
+        int result = Factorial(n);
+        Console.WriteLine($"Факториал числа {n} равен {result}");
+    }
+
+    static int Factorial(int n)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);
+        }
+    }
+}
